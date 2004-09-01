d5b958549e1f163449fab1e199c7e6a55d364d78
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2004-09-01 13:36:31 +0000

    fixes to use 2.4/2.6

--- v3.x/posix/kern/condtest.c
+++ v3.x/posix/kern/condtest.c
@@ -22,7 +22,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 #include <rtai_posix.h>
 
 MODULE_LICENSE("GPL");
-EXPORT_NO_SYMBOLS;
 
 #define TICK 1000000
 
--- v3.x/posix/kern/jepplin.c
+++ v3.x/posix/kern/jepplin.c
@@ -21,6 +21,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 #include <linux/module.h>
 #include <linux/errno.h>
 
+#include <asm/fcntl.h>
+
 #include <rtai_posix.h>
 #include <rtai_mq.h>
 
