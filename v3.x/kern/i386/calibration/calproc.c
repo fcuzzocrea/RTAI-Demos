906cab36aaaf8f0f733eacabb028d35ae28163ea
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2006-02-20 08:11:43 +0000

    *** empty log message ***

--- v3.x/kern/i386/calibration/calproc.c
+++ v3.x/kern/i386/calibration/calproc.c
@@ -92,7 +92,7 @@ int main(void)
 		printf("- Both cpu and apic frequency calibration.\n");
 		printf("- bus Lock check.\n");
 		printf("- End.\n");
-		printf("<Keypress 8 or the only Upper case char in each line for a choice>\n");
+		printf("< For a choice: keypress 8 or the only Upper case character in each line. >\n");
 		str[0] = getkey();
 		switch(command = tolower(str[0])) {
 
