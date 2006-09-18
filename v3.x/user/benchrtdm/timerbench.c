d4ce2fdd333fd196ee3a72b6a835f68c126b78a9
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2006-09-18 07:37:30 +0000

    *** empty log message ***

--- v3.x/user/benchrtdm/timerbench.c
+++ v3.x/user/benchrtdm/timerbench.c
@@ -26,6 +26,19 @@
 #include "rttesting.h"
 #include "xntimer.h"
 
+#ifndef container_of
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:        the pointer to the member.
+ * @type:       the type of the container struct this is embedded in.
+ * @member:     the name of the member within the struct.
+ *
+ */
+#define container_of(ptr, type, member) ({                      \
+        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+        (type *)( (char *)__mptr - offsetof(type,member) );})
+#endif
+
 struct rt_tmbench_context {
     int                             mode;
     unsigned long                   period;
