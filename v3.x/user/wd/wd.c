09429cc51d9cc68b950c1e5caef8fd1611a5b136
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2007-09-07 12:49:09 +0000

    *** empty log message ***

--- v3.x/user/wd/wd.c
+++ v3.x/user/wd/wd.c
@@ -21,61 +21,23 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 #include <stdlib.h>
 #include <rtai_lxrt.h>
 
-static RT_TASK *Slow_Task;
-static RT_TASK *Fast_Task;
+static RTIME wd_period_ns = 100000000; // period of WD = 1/10 sec. 
 
-static void *slow_fun(void *arg)
-{
-        if (!(Slow_Task = rt_task_init_schmod(nam2num("SLWTSK"), 3, 0, 0, SCHED_FIFO, 0x1))) {
-                printf("CANNOT INIT SLOW TASK\n");
-                exit(1);
-        }
+int main (void)
+{ 
+	RT_TASK *rt_task;
+	RTIME task_period; 
 
-	mlockall(MCL_CURRENT | MCL_FUTURE);
+	mlockall(MCL_CURRENT|MCL_FUTURE); 
+	rt_allow_nonroot_hrt();
+	rt_task = rt_task_init_schmod(nam2num("RTRecv"), 1, 0, 0, SCHED_FIFO, 0xF);
+	task_period = nano2count(30*wd_period_ns); 
+
 	rt_make_hard_real_time();
-        rt_printk("SLOW\n");
-        while (1);
+	rt_task_make_periodic(rt_task, rt_get_time(), task_period);
+	while (1);
 	rt_make_soft_real_time();
-	rt_task_delete(Slow_Task);
-	return 0;
-}                                        
 
-static void *fast_fun(void *arg) 
-{                             
-        if (!(Fast_Task = rt_task_init_schmod(nam2num("FSTSK"), 2, 0, 0, SCHED_FIFO, 0x1))) {
-                printf("CANNOT INIT FAST TASK\n");
-                exit(1);
-        }
-
-	mlockall(MCL_CURRENT | MCL_FUTURE);
-	rt_make_hard_real_time();
-        rt_printk("FAST\n");
-	rt_sleep(nano2count(300000000));
-	rt_task_suspend(Slow_Task);
-	rt_task_delete(Slow_Task);
-	rt_make_soft_real_time();
-	rt_task_delete(Fast_Task);
-	return 0;
-}
-
-static pthread_t fast_thread, slow_thread;
-
-int main(void)
-{
-	RT_TASK *Main_Task;
-
-        if (!(Main_Task = rt_task_init_schmod(nam2num("MNTSK"), 1, 0, 0, SCHED_FIFO, 0x1))) {
-                printf("CANNOT INIT MAIN TASK\n");
-                exit(1);
-        }
-	rt_set_oneshot_mode();
-	start_rt_timer(0);
-
-	pthread_create(&fast_thread, NULL, fast_fun, NULL);
-	rt_sleep(nano2count(100000000));
-	pthread_create(&slow_thread, NULL, slow_fun, NULL);
-	pause();
-	stop_rt_timer();	
-	rt_task_delete(Main_Task);
-	return 0;
+	rt_task_delete(rt_task);
+	return 0; 
 }
