d3cc54be969045bcfc74119ad0c3ce30ffba4644
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2008-06-05 22:20:22 +0000

    *** empty log message ***

--- v3.x/user/preempt/preempt.c
+++ v3.x/user/preempt/preempt.c
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 #include <rtai_mbx.h>
 #include <rtai_msg.h>
 
-#define CPUMAP 0x1
+#define CPUMAP 0xF
 
 #define USEDFRAC 20  // in %
 
