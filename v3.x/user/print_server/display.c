6bae326d657cb55e153b4e02ab565ebd87155b1f
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2008-06-25 07:42:49 +0000

    *** empty log message ***

---
+++ v3.x/user/print_server/display.c
@@ -0,0 +1,48 @@
+/*
+COPYRIGHT (C) 2008  Paolo Mantegazza (mantegazza@aero.polimi.it)
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/poll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(void)
+{
+	int sock;
+	struct sockaddr_in SPRT_ADDR;
+	char buf[100];
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	bzero(&SPRT_ADDR, sizeof(struct sockaddr_in));
+        SPRT_ADDR.sin_family = AF_INET;
+	SPRT_ADDR.sin_port = htons(5000);
+        SPRT_ADDR.sin_addr.s_addr = htonl(INADDR_ANY);
+	bind(sock, (struct sockaddr *)&SPRT_ADDR, sizeof(struct sockaddr_in));
+
+	while (1) {
+		recvfrom(sock, buf, 100, 0, NULL, NULL);
+		printf("%s", buf);
+	}
+	close(sock);
+	return 0;
+}
