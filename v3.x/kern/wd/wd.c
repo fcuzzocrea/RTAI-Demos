7fda12cdfcc1f0e268226ede8ea6c2c45f78b1e7
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2008-09-27 12:37:53 +0000

    *** empty log message ***

--- v3.x/kern/wd/wd.c
+++ v3.x/kern/wd/wd.c
@@ -22,26 +22,39 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
 #include <rtai.h>
 #include <rtai_sched.h>
+#include <rtai_sem.h>
+#include <rtai_msg.h>
 
 MODULE_LICENSE("GPL");
 
-static RT_TASK task;
+static RT_TASK *task;
 
-#define TASK_PERIOD 1000000 // ns
+#define PERIOD 1000000 // ns
 
-static void fun(long none)
+static void fun(long usemsg)
 {
-	while (1);
+	if (rt_is_hard_timer_running()) {
+		rt_task_make_periodic(task, rt_get_time(), nano2count(PERIOD));
+	}
+	if (usemsg) {
+		unsigned long msg;
+		while (!rt_receive_if(NULL, &msg));
+	} else {
+		SEM *sem;
+		sem = rt_typed_named_sem_init("KILSEM", 0, CNT_SEM);
+		while (rt_sem_wait_if(sem) <= 0);
+		rt_named_sem_delete(sem);
+	}
 }
 
 int init_module(void)
 {
-	rt_task_init(&task, fun, 0, 8000, 1, 0, 0);
-	rt_task_make_periodic(&task, rt_get_time(), nano2count(TASK_PERIOD));
+	task = rt_named_task_init_cpuid("LOOPER", fun, 1, 8000, 1, 0, 0, 1);
+	rt_task_resume(task);
 	return 0;
 }
 
 void cleanup_module(void)
 {
-	rt_task_delete(&task);
+	rt_named_task_delete(task);
 }
