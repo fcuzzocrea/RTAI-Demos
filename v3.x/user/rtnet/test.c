90c20c662d2808f1001c687fc9ea577a8df2508f
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2008-10-12 14:17:06 +0000

    *** empty log message ***

--- v3.x/user/rtnet/test.c
+++ v3.x/user/rtnet/test.c
@@ -20,7 +20,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
  This example might appear a bit bloated as, being based on threads, it 
  could have shared anything. We have elected to not use global data just
  to show a few more RTAI services working together, e.g.: synchronization
- through intertask messages and objects handles through registered infos.
+ and timing through intertask messages and objects handles through registered
+ infos.
 */
 
 #include <pthread.h>
@@ -70,16 +71,19 @@ static void *sender(void *arg)
 		printf("Cannot initialise the sender task\n");
 		exit(1);
 	}
-
+	if (((sock = rt_dev_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))) == -1) {
+		printf("Error opening UDP/IP socket: %lu\n", sock);
+		exit(1);
+	}
 	transmit_addr.sin_family      = AF_INET;
 	transmit_addr.sin_port        = htons(PORT);
 	transmit_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	rt_rpc(rt_get_adr(nam2num("MNTSK")), 0UL, &sock);
+	rt_rpc(rt_get_adr(nam2num("MNTSK")), 0UL, &slen);
 
 	ECHO("Transmitter task initialised\n");
 	rt_make_hard_real_time();
 
-	while(!rt_receive_if(rt_get_adr(nam2num("MNTSK")), &slen)) {
+	while(rt_receive_timed(rt_get_adr(nam2num("MNTSK")), &slen, nano2count(WORKCYCLE)) != rt_get_adr(nam2num("MNTSK"))) {
 		++tx_samp.cnt;
 		tx_samp.tx = rt_get_time_ns();
 		slen = rt_dev_sendto(sock, (void *)&tx_samp, offsetof(struct sample, rx), 0, (struct sockaddr*)&transmit_addr, sizeof(transmit_addr));
@@ -88,12 +92,14 @@ static void *sender(void *arg)
 		} else {
 			ECHO("SENDER SENT %lu INSTEAD OF %d\n", slen, offsetof(struct sample, rx));
 		}
-		rt_sleep(nano2count(WORKCYCLE));
+//		rt_sleep(nano2count(WORKCYCLE)); timing cared by timed while
 	}
 
 	rt_task_masked_unblock(rt_get_adr(nam2num("RXTSK")), ~RT_SCHED_READY);
 	rt_dev_sendto(sock, (void *)&tx_samp, offsetof(struct sample, rx), 0, (struct sockaddr*)&transmit_addr, sizeof(transmit_addr)); // not needed, just to be safe
 	rt_make_soft_real_time();
+	rt_dev_close(sock);
+	rt_dev_shutdown(sock, SHUT_RDWR);
 	rt_thread_delete(Sender_Task);
 	printf("Transmitter exiting\n");
 	return 0;
@@ -102,11 +108,12 @@ static void *sender(void *arg)
 static void *receiver(void *arg)
 {
 	RT_TASK *Receiver_Task;
+	struct sockaddr_in local_addr;
 	MBX *mbx = rt_get_adr(nam2num("MYMBX"));
 	struct sample rx_samp;
 	long cnt = 0;
 	unsigned long sock, rlen;
-	int usetimeout = 0;
+	int broadcast = 1;
 	socklen_t fromlen;
 	struct sockaddr_in receive_addr;
 	fromlen = sizeof(receive_addr);
@@ -116,8 +123,29 @@ static void *receiver(void *arg)
 		printf("Cannot initialise the receiver task\n");
 		exit(1);
 	}
+	if (((sock = rt_dev_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))) == -1) {
+		printf("Error opening UDP/IP socket: %lu\n", sock);
+		exit(1);
+	}
+	if (rt_dev_setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast)) == -1) {
+		printf("Can't set broadcast options\n");
+		rt_dev_close(sock);
+		exit(1);
+	}
+	if (USESEL) {
+		int64_t timeout = -1;
+		rt_dev_ioctl(sock, RTNET_RTIOC_TIMEOUT, &timeout);
+	}
+	local_addr.sin_family      = AF_INET;
+	local_addr.sin_port        = htons(PORT);
+	local_addr.sin_addr.s_addr = INADDR_ANY;
 
-	rt_rpc(rt_get_adr(nam2num("MNTSK")), 0UL, &sock);
+	if (rt_dev_bind(sock, (struct sockaddr *)&local_addr, sizeof(struct sockaddr_in)) == -1) {
+		ECHO("Can't configure the network socket");
+		rt_dev_close(sock);
+		exit(1);
+	}
+	rt_rpc(rt_get_adr(nam2num("MNTSK")), 0UL, &rlen);
 
 	ECHO("Receiver task initialised\n");
 	rt_make_hard_real_time();
@@ -125,6 +153,7 @@ static void *receiver(void *arg)
 	while(!rt_receive_if(rt_get_adr(nam2num("MNTSK")), &rlen)) {
 		int ready = 0;
 #if USESEL
+		static int usetimeout = 0;
 		RTIME timeout;
 		usetimeout = 1 - usetimeout;
 		timeout = usetimeout ? TIMEOUT : 0;
@@ -139,7 +168,7 @@ static void *receiver(void *arg)
 		}
 #endif
 		if (!USESEL || (ready > 0 && FD_ISSET(sock, &rxfds))) {
-			rlen = rt_dev_recvfrom(sock, (void *)&rx_samp, sizeof(struct sample), 0, (struct sockaddr*) &receive_addr, &fromlen);
+			rlen = rt_dev_recvfrom(sock, (void *)&rx_samp, sizeof(struct sample), 0, (struct sockaddr*)&receive_addr, &fromlen);
 			if (rlen == offsetof(struct sample, rx)) {
 				rx_samp.rx = rt_get_time_ns();
 				rt_mbx_send_if(mbx, &rx_samp, sizeof(rx_samp));
@@ -151,6 +180,8 @@ static void *receiver(void *arg)
 	}
 
 	rt_make_soft_real_time();
+	rt_dev_close(sock);
+	rt_dev_shutdown(sock, SHUT_RDWR);
 	rt_thread_delete(Receiver_Task);
 	ECHO("Receiver exiting\n");
 	return 0;
@@ -161,15 +192,12 @@ static void endme(int unused) { end = 1; }
 
 int main(void)
 {
+	RT_TASK *Main_Task;
 	MBX *mbx;
-	struct sockaddr_in local_addr;
 	pthread_t sender_thread;
 	pthread_t receiver_thread;
-	RT_TASK *Main_Task;
-	unsigned long cnt = 0;
+	unsigned long cnt = 0, misd;
 	struct sample frombx;
-	unsigned long sock;
-	int broadcast = 1;
 
 	signal(SIGHUP, endme);
 	signal(SIGINT, endme);
@@ -187,56 +215,33 @@ int main(void)
 		printf("Cannot create the mailbox\n");
 		exit(1);
 	}
-	if (((sock = rt_dev_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))) == -1) {
-		printf("Error opening UDP/IP socket: %lu\n", sock);
-		exit(1);
-	}
-	if (rt_dev_setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast)) == -1) {
-		printf("Can't set broadcast options\n");
-		rt_dev_close(sock);
-		exit(1);
-	}
-	if (USESEL) {
-		int64_t timeout = -1;
-		rt_dev_ioctl(sock, RTNET_RTIOC_TIMEOUT, &timeout);
-	}
-
-	local_addr.sin_family      = AF_INET;
-	local_addr.sin_port        = htons(PORT);
-	local_addr.sin_addr.s_addr = INADDR_ANY;
-
-	if (rt_dev_bind(sock, (struct sockaddr *) &local_addr, sizeof(struct sockaddr_in)) == -1) {
-		ECHO("Can't configure the network socket");
-		rt_dev_close(sock);
-		exit(1);
-	}
-
 	sender_thread = rt_thread_create(sender, NULL, 0);
-	rt_return(rt_receive(NULL, &cnt), sock); 
+	rt_return(rt_receive(NULL, &cnt), 0UL); 
 	receiver_thread = rt_thread_create(receiver, NULL, 0);
-	rt_return(rt_receive(NULL, &cnt), sock); 
+	rt_return(rt_receive(NULL, &cnt), 0UL); 
 
 	mlockall(MCL_CURRENT | MCL_FUTURE);
 	rt_make_hard_real_time();
 
 #if USEMBX
 	while (!end) {
-		rt_mbx_receive(mbx, (void *)&frombx, sizeof(frombx));
-		ECHO("MAIN FROM MBX %lu %lld %lld\n", ++cnt, frombx.tx, frombx.rx);
+		if (!(misd = rt_mbx_receive(mbx, (void *)&frombx, sizeof(frombx)))) {
+			ECHO("MAIN FROM MBX %lu-%lu %lld %lld\n", ++cnt, frombx.cnt, frombx.tx, frombx.rx);
+		} else {
+			ECHO("MAIN MBX ASKED FOR %d MISSED %lu\n", sizeof(frombx), misd);
+		}
 	}
 #else
 	pause();
 #endif
+
 	rt_send(rt_get_adr(nam2num("TXTSK")), 0UL); 
 	rt_send(rt_get_adr(nam2num("RXTSK")), 0UL); 
-
 	rt_thread_join(sender_thread);
 	rt_thread_join(receiver_thread);
-	rt_dev_close(sock);
-	rt_dev_shutdown(sock, SHUT_RDWR);
 	stop_rt_timer();
-	rt_mbx_delete(mbx);
 	rt_make_soft_real_time();
+	rt_mbx_delete(mbx);
 	rt_thread_delete(Main_Task);
 	return 0;
 }
