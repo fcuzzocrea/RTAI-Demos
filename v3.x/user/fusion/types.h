5a9144ca6cd894c48f8150a634b7aad9d2023fd4
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2010-04-24 14:14:32 +0000

    *** empty log message ***

--- v3.x/user/fusion/types.h
+++ v3.x/user/fusion/types.h
@@ -25,7 +25,7 @@
 
 #define SEM_ERR     (0xFFFF)
 #define SEM_TIMOUT  (0xFFFE)
-#define MSG_ERR     ((void *)0xFFFF)
+#define MSG_ERR     ((void *)0x3FFFFFFF)
 
 #define PRIO_Q    0 
 #define FIFO_Q    4 
