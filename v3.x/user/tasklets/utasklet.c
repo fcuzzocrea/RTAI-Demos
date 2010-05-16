d132e9724eb48ebb40620c2473bb1d300df20761
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2010-05-16 17:44:10 +0000

    *** empty log message ***

--- v3.x/user/tasklets/utasklet.c
+++ v3.x/user/tasklets/utasklet.c
@@ -28,7 +28,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
 #include <rtai_tasklets.h>
 
-#define LOOPS 100
+#define LOOPS   100
+#define PERIOD  10000000
 
 static volatile int end, data = LOOPS;
 
@@ -47,20 +48,18 @@ static void tasklet_handler(unsigned long data)
 
 int main(void)
 {
-	struct sched_param mysched;
-
-	mysched.sched_priority = sched_get_priority_max(SCHED_FIFO);
-	if( sched_setscheduler( 0, SCHED_FIFO, &mysched ) == -1 ) {
-		puts(" ERROR IN SETTING THE SCHEDULER UP");
-		perror("errno");
-		exit(0);
- 	}       
+	start_rt_timer(0);
+	rt_task_init_schmod(rt_get_name(0), 10, 0, 0, SCHED_OTHER, 0xF);
 	mlockall(MCL_CURRENT | MCL_FUTURE);
 
 	tasklet = rt_init_tasklet();
 	rt_insert_tasklet(tasklet, 0, tasklet_handler, (unsigned long)&data, nam2num("TSKLET"), 1);
-	while(!end) sleep(1);
+	while(!end) {
+		rt_exec_tasklet(tasklet);
+		rt_sleep(nano2count(PERIOD));
+	}
 	rt_remove_tasklet(tasklet);
 	rt_delete_tasklet(tasklet);
+	rt_task_delete(NULL);
 	return 0;
 }
