99036b40848ccde137e030c785b50b7fecc5b062
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2017-04-22 21:06:46 +0000

    *** empty log message ***

--- v3.x/kern/netrpc/task_code.c
+++ v3.x/kern/netrpc/task_code.c
@@ -20,6 +20,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+MODULE_LICENSE("GPL");
+
 #include <rtai_netrpc.h>
 
 static char *ComNode = "127.0.0.1";
