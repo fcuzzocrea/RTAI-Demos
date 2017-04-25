ac673ed7c32ffda1055296f84ad76a9f7ae7b40a
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2017-04-25 13:53:03 +0000

    *** empty log message ***

--- v3.x/kern/i386/lnxshintr/lnxshintr.c
+++ v3.x/kern/i386/lnxshintr/lnxshintr.c
@@ -4,23 +4,23 @@
 
 #define MAX_NIRQ 10
 static int nirq;
-static int irq[] = {1, 12, 19};
-static int level[] = {0, 0, 0};
-static int cnt[] = {0, 0, 0};
+static int irq[] = {0, 1, 12, 26};
+static int level[] = {0, 0, 0, 0};
+static int cnt[] = {0, 0, 0, 0};
 static char post_handler[MAX_NIRQ][20];
 static void *dev_id[MAX_NIRQ];
 
 static void rtai_handler(int irqa, void *idx)
 {
-	int i;
-	i = (int)idx;
+	long i;
+	i = (long)idx;
 	cnt[i]++;	
 	rt_pend_linux_irq(irq[i]);
 }
 
 static int linux_post_handler(int irqa, void *dev_id, struct pt_regs *regs)
 {
-	int i = (int)dev_id;
+	long i = (long)dev_id;
 	if (level[i-1]) rt_enable_irq(irqa);
 	rt_printk("LINUX IRQ %d: %d %d %d\n", i, cnt[i-1], irqa, irq[i-1]);
 	return 1;
@@ -28,14 +28,14 @@ static int linux_post_handler(int irqa, void *dev_id, struct pt_regs *regs)
 
 int init_module(void)
 {
-	int i;
+	long i;
 	nirq = sizeof(irq)/sizeof(int);
 	if (nirq >= MAX_NIRQ) {
 		printk("SET MAX_NIRQ MACRO AT LEAST TO %d.\n", nirq);
 		return 1;
 	}
 	for (i = 0; i < nirq; i++) {
-		snprintf(post_handler[i], sizeof(post_handler[i]), "POST_HANDLER%d", i + 1);
+		snprintf(post_handler[i], sizeof(post_handler[i]), "POST_HANDLER%ld", i + 1);
 		dev_id[i] = (void *)(i+1);
 		rt_request_linux_irq(irq[i], linux_post_handler, post_handler[i], dev_id[i]);
 		if (level[i]) rt_set_irq_ack(irq[i], (void *)rt_disable_irq);
